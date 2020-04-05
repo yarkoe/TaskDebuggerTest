56bf9397898cf79a9b98bfe2cdd4f1119ae2d6e8
Koekin Yaroslav <ykoekin@gmail.com>
2020-04-05 20:41:07 +0300

    Added task scheduler and thread pool

--- TaskDebuggerTest/GraphParallelProcessor.cs
+++ TaskDebuggerTest/GraphParallelProcessor.cs
@@ -1,43 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.AccessControl;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskDebuggerTest
 {
     public static class GraphParallelProcessor
     {
-        private static object lockObject = new object();
+        private static readonly MyTaskScheduler _taskScheduler = new MyTaskScheduler(); 
+        
+        private static readonly object _lockObject = new object();
         
         public static Task ProcessInParallelAsync(Graph<Action> actions)
         {
+            if (actions.HasCycle())
+                throw new Exception("Action graph has cycle");
+            
             var transposedActions = Graph<Action>.TransposeGraph(actions);
             var initialVertices = transposedActions.GetLeaves();
 
             var initialTasks = initialVertices.Select(initialVertex => 
-                Task.Run(() => ProcessVertexInParallelAsync(actions, transposedActions, initialVertex))).ToList();
+                Task.Factory.StartNew(() => 
+                    ProcessVertexInParallelAsync(actions, transposedActions, initialVertex),
+                    CancellationToken.None, TaskCreationOptions.None, _taskScheduler).Unwrap()).ToList();
 
             return Task.WhenAll(initialTasks);
         }
 
         private static async Task ProcessVertexInParallelAsync(Graph<Action> actions, Graph<Action> transposedActions, Graph<Action>.Vertex vertex)
         {
-            var actionTask = Task.Run(() => vertex.Value());
+            var actionTask = Task.Factory.StartNew(() => vertex.Value(), 
+                CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
 
             await actionTask;
 
             var nextVertices = new List<Task>();
             foreach (var toVertex in actions.FindVertexAdjacencyList(vertex.Id).Value)
             {
-                lock (lockObject)
+                lock (_lockObject)
                 {
                     transposedActions.RemoveEdge(toVertex.Id, vertex.Id);
 
                     if (transposedActions.FindVertexAdjacencyList(toVertex.Id).Value.Count == 0)
                     {
-                        nextVertices.Add(Task.Run(() =>
-                            ProcessVertexInParallelAsync(actions, transposedActions, toVertex)));
+                        nextVertices.Add(Task.Factory.StartNew(() =>
+                            ProcessVertexInParallelAsync(actions, transposedActions, toVertex),
+                            CancellationToken.None, TaskCreationOptions.None, _taskScheduler).Unwrap());
                     }
                 }
             }
---
+++ TaskDebuggerTest/MyTaskScheduler.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDebuggerTest
+{
+    public class MyTaskScheduler : TaskScheduler, IDisposable
+    {
+        private const int ThreadNumber = 20;
+        private readonly List<Task> _scheduledTasks = new List<Task>();
+        private readonly MyThreadPool _threadPool;
+
+        public MyTaskScheduler()
+        {
+            _threadPool = new MyThreadPool(ThreadNumber, this);
+        }
+        
+        protected override void QueueTask(Task task)
+        {
+            _scheduledTasks.Add(task);
+            _threadPool.AddTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) => false;
+
+        protected override IEnumerable<Task> GetScheduledTasks() => _scheduledTasks;
+
+        public void Dispose() => _threadPool.Shutdown();
+
+        private class MyThreadPool
+        {
+            private readonly object _lockObject = new object();
+            private readonly object _eventLockObject = new object();
+
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+            private CancellationToken _token;
+
+            private readonly int _threadCount = 0;
+            private int _stopCount = 0;
+
+            private readonly ConcurrentQueue<Task> _taskQueue = new ConcurrentQueue<Task>();
+            private Thread[] _threads;
+            private readonly ManualResetEvent _threadEvent = new ManualResetEvent(false);
+            private readonly ManualResetEvent _allstopEvent = new ManualResetEvent(false);
+
+            private readonly MyTaskScheduler _myTaskScheduler;
+            
+            public MyThreadPool(int n, MyTaskScheduler taskScheduler)
+            {
+                _myTaskScheduler = taskScheduler;
+                
+                _threadCount = n;
+
+                _token = _cts.Token;
+
+                _threads = new Thread[_threadCount];
+
+                for (var i = 0; i < _threadCount; ++i)
+                {
+                    _threads[i] = new Thread(Run) { IsBackground = true };
+
+                }
+
+                foreach (var thread in _threads)
+                {
+                    thread.Start();
+                }
+            }
+            
+            public void AddTask(Task task)
+            {
+                AddToQueue(task);
+            }
+            
+            private void AddToQueue(Task task)
+            {
+                lock (_eventLockObject)
+                {
+                    _taskQueue.Enqueue(task);
+
+                    _threadEvent.Set();
+                }
+            }
+            
+            private void Run()
+            {
+                while (true)
+                {
+                    _threadEvent.WaitOne();
+
+                    lock (_eventLockObject)
+                    {
+                        if (_taskQueue.Count == 1)
+                        {
+                            _threadEvent.Reset();
+                        }
+                    }
+
+                    if (_token.IsCancellationRequested)
+                    {
+                        _threadEvent.Set();
+
+                        lock (_lockObject)
+                        {
+                            _stopCount++;
+                        }
+
+                        if (_stopCount == _threadCount)
+                            _allstopEvent.Set();
+
+                        return;
+                    }
+
+                    Task task = null;
+                    lock (_lockObject)
+                    {
+                        if (_taskQueue.Count != 0)
+                        {
+                            _taskQueue.TryDequeue(out task);
+                        }
+                    }
+
+                    if (task == null) continue;
+                    
+                    
+                    _myTaskScheduler.TryExecuteTask(task);
+                }
+            }
+
+            public void Shutdown()
+            {
+                _cts.Cancel();
+                _cts.Dispose();
+
+                _threadEvent.Set();
+
+                _allstopEvent.WaitOne();
+            }
+        }
+    }
+}
